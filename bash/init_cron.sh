63a88975090fa57f2df330cbd63f4be774ccf8d6
Halil Karakose <halilkarakose@gmail.com>
2023-07-20 15:54:30 +0300

    s3 clean task

--- bash/init_cron.sh
+++ bash/init_cron.sh
@@ -7,6 +7,10 @@ source config.sh
 BACKUP_SCRIPT_PATH="$COBACKUP_HOME/bash/backup.sh"
 chmod 500 $BACKUP_SCRIPT_PATH
 
+# Set the backup script path
+S3_SCRIPT_PATH="$COBACKUP_HOME/bash/clean_s3.sh"
+chmod 500 $S3_SCRIPT_PATH
+
 # Create a temporary cron file
 CRON_FILE=$(mktemp)
 
@@ -15,6 +19,9 @@ crontab -l > "$CRON_FILE" 2>/dev/null
 
 # Append the new cron job entry
 echo "*/10 * * * * cd $(echo $COBACKUP_HOME)/bash && $(echo $BACKUP_SCRIPT_PATH)" >> "$CRON_FILE"
+
+# Append the new cron job entry
+echo "0 0 0 * * cd $(echo $COBACKUP_HOME)/bash && $(echo $S3_SCRIPT_PATH)" >> "$CRON_FILE"
 
 # Install the updated cron file
 crontab "$CRON_FILE"
